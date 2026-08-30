01739a37a89a7d912f9097b19879f0988be7864b
Aaryan Goswami <Aaryangoswami1@outlook.com>
2026-08-30 03:21:06 +0000

    Time: 0 ms (100.00%) | Memory: 46.6 MB (80.86%) - LeetX Squads

---
+++ Easy/0026-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,19 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 1;
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[j - 1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+}
