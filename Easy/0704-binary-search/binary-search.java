a3a186d513c191d35b5f19c2d31c37f799bfbd8b
Aaryan Goswami <Aaryangoswami1@outlook.com>
2026-09-16 17:56:02 +0000

    Time: 0 ms (100.00%) | Memory: 48.1 MB (84.39%) - LeetX Squads

---
+++ Easy/0704-binary-search/binary-search.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int mid, low = 0,high = nums.length-1;
+        while(low<=high){
+            mid = low + (high-low)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                high = mid-1;
+            else if(nums[mid] < target)
+                low = mid+ 1;
+        }
+        return -1;
+    }
+}
