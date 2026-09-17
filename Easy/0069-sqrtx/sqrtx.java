6ee44961fdb2b47f520ec1f534253175cc520863
Aaryan Goswami <Aaryangoswami1@outlook.com>
2026-09-17 16:02:54 +0000

    Time: 1 ms (99.77%) | Memory: 42.5 MB (78.52%) - LeetX Squads

---
+++ Easy/0069-sqrtx/sqrtx.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2) return x;
+        int mid ,low =1 , high = x/2;
+        while(low<=high){
+            mid = low + (high - low)/2;
+            if((long) mid*mid <=x)
+                low = mid +1;
+            else
+                high = mid -1;
+        }
+        return high ;
+    }
+}
