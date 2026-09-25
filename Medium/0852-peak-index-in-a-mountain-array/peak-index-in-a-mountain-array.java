84c756bbd333c4afe631f53aacd4570e5fb2a4c7
Aaryan Goswami <Aaryangoswami1@outlook.com>
2026-09-25 15:00:47 +0000

    Time: 0 ms (100.00%) | Memory: 80.4 MB (38.37%) - LeetX Squads

---
+++ Medium/0852-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int mid,low=0,high=arr.length-1;
+        while(low<=high){
+            mid = low+(high-low)/2;
+            if(arr[mid]<arr[mid+1])
+                low = mid+1;
+                else
+                high = mid-1;
+        }
+        return low;
+    }
+}
